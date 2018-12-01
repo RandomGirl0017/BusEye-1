f3c5f4e4a968300caa17e01f8b67ab89dd3e87a0
Ramon <kindreds09@gmail.com>
2018-12-01 10:25:40 -0200

    Codigo para fazer fragmentos no menu inferior

--- app/src/main/java/com/example/lucas/buseye/view/SearchView.java
+++ app/src/main/java/com/example/lucas/buseye/view/SearchView.java
@@ -6,6 +6,7 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
@@ -109,7 +110,37 @@ public class SearchView extends AppCompatActivity implements NavigationView.OnNa
         BottomNavigationView bottomNav = findViewById(R.id.bottom_nav);
         //bottomNav.setOnNavigationItemSelectedListener(navListener);
 
+            //I added this if statement to keep the selected fragment when rotating the device
+            /*if (savedInstanceState == null) {
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new HomeFragment()).commit();
+            }
+        }
 
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    Fragment selectedFragment = null;
+
+                    switch (item.getItemId()) {
+                        case R.id.nav_home:
+                            selectedFragment = new HomeFragment();
+                            break;
+                        case R.id.nav_favorites:
+                            selectedFragment = new FavoritesFragment();
+                            break;
+                        case R.id.nav_search:
+                            selectedFragment = new SearchFragment();
+                            break;
+                    }
+
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                            selectedFragment).commit();
+
+                    return true;
+                }
+*/
 
         //FLOATING BUTTON
 
